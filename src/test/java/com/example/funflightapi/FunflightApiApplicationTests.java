cdab4d1636fa34767be5c3cef6048c0c0758b593
VerySadOtaku <german.sichenikov@gmail.com>
2024-02-10 13:17:14 +0200

    first commit

---
+++ src/test/java/com/example/funflightapi/FunflightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funflightapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunflightApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
